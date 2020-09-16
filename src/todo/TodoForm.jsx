19c046a9e96ee4f983ecc7ccfe2a83dbca5129f1
Denilson Donolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-09-16 11:22:12 -0300

    Finalizado sem redux

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -2,30 +2,47 @@ import React from "react";
 import Grid from "../template/Grid";
 import IconButton from "../template/IconButton";
 
-export default (props) => (
-  <div role="form" className="todoForm row">
-    <Grid cols="12 9 10">
-      <input
-        id="description"
-        className="form-control"
-        placeholder="Adicione uma tarefa"
-        value={props.description}
-        onChange={props.handleChange}
-      />
-    </Grid>
-    <Grid cols="12 3 2">
-      <IconButton
-        // eslint-disable-next-line
-        style="primary"
-        icon="plus"
-        onClick={props.handleAdd}
-      />
-      <IconButton
-        // eslint-disable-next-line
-        style="info"
-        icon="search"
-        onClick={props.handleSearch}
-      />
-    </Grid>
-  </div>
-);
+export default (props) => {
+  const keyHandler = (e) => {
+    if (e.key === "Enter") {
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    } else if (e.key === "Escape") {
+      props.handleClear();
+    }
+  };
+
+  return (
+    <div role="form" className="todoForm row">
+      <Grid cols="12 8 9">
+        <input
+          id="description"
+          className="form-control"
+          placeholder="Adicione uma tarefa"
+          value={props.description}
+          onChange={props.handleChange}
+          onKeyUp={keyHandler}
+        />
+      </Grid>
+      <Grid cols="12 4 3">
+        <IconButton
+          // eslint-disable-next-line
+          style="primary"
+          icon="plus"
+          onClick={props.handleAdd}
+        />
+        <IconButton
+          // eslint-disable-next-line
+          style="info"
+          icon="search"
+          onClick={props.handleSearch}
+        />
+        <IconButton
+          // eslint-disable-next-line
+          style="light"
+          icon="close"
+          onClick={props.handleClear}
+        />
+      </Grid>
+    </div>
+  );
+};
