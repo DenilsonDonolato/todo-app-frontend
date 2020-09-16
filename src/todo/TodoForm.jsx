5a80c0b1a500efeb21ecbdb5b2af04a344968d5b
Denilson Donolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-09-16 09:58:18 -0300

    Post de nova tarefa

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -9,10 +9,17 @@ export default (props) => (
         id="description"
         className="form-control"
         placeholder="Adicione uma tarefa"
+        value={props.description}
+        onChange={props.handleChange}
       />
     </Grid>
     <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+      <IconButton
+        // eslint-disable-next-line
+        style="primary"
+        icon="plus"
+        onClick={props.handleAdd}
+      />
     </Grid>
   </div>
 );
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import PageHeader from "../template/PageHeader";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
+const URL = "http://localhost:3003/api/todos";
+
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = { description: "", list: "" };
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
   }
 
   handleAdd() {
-    console.log(this);
+    const description = this.state.description;
+    axios
+      .post(URL, { description })
+      .then((resp) => this.setState({ description: "" }));
   }
 
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm handleAdd={this.handleAdd} />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     );
