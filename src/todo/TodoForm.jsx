a285924e00e071fcf271b4c96f31b77d7356eb13
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2021-01-17 17:50:15 -0300

    redux promise

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,59 +1,70 @@
-import React from "react";
+import React, {Component} from "react";
 import { connect } from "react-redux";
 import Grid from "../template/Grid";
 import IconButton from "../template/IconButton";
 import { bindActionCreators } from "redux";
-import { changeDescription } from "./todoActions";
+import { changeDescription, search } from "./todoActions";
 
-const TodoForm = (props) => {
-  const keyHandler = (e) => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.keyHandler = this.keyHandler.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.search();
+  }
+
+  keyHandler(e) {
     if (e.key === "Enter") {
-      e.shiftKey ? props.handleSearch() : props.handleAdd();
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
     } else if (e.key === "Escape") {
-      props.handleClear();
+      this.props.handleClear();
     }
   };
 
-  return (
-    <div role="form" className="todoForm row">
-      <Grid cols="12 8 9">
-        <input
-          id="description"
-          className="form-control"
-          placeholder="Adicione uma tarefa"
-          value={props.description}
-          onChange={props.changeDescription}
-          onKeyUp={keyHandler}
-        />
-      </Grid>
-      <Grid cols="12 4 3">
-        <IconButton
-          // eslint-disable-next-line
-          style="primary"
-          icon="plus"
-          onClick={props.handleAdd}
-        />
-        <IconButton
-          // eslint-disable-next-line
-          style="info"
-          icon="search"
-          onClick={props.handleSearch}
-        />
-        <IconButton
-          // eslint-disable-next-line
-          style="light"
-          icon="close"
-          onClick={props.handleClear}
-        />
-      </Grid>
-    </div>
-  );
-};
+  render(){
+    return (
+      <div role="form" className="todoForm row">
+        <Grid cols="12 8 9">
+          <input
+            id="description"
+            className="form-control"
+            placeholder="Adicione uma tarefa"
+            value={this.props.description}
+            onChange={this.props.changeDescription}
+            onKeyUp={this.keyHandler}
+          />
+        </Grid>
+        <Grid cols="12 4 3">
+          <IconButton
+            // eslint-disable-next-line
+            style="primary"
+            icon="plus"
+            onClick={this.props.handleAdd}
+          />
+          <IconButton
+            // eslint-disable-next-line
+            style="info"
+            icon="search"
+            onClick={this.props.handleSearch}
+          />
+          <IconButton
+            // eslint-disable-next-line
+            style="light"
+            icon="close"
+            onClick={this.props.handleClear}
+          />
+        </Grid>
+      </div>
+    );  
+  }
+}
 
 function mapStateToProps(state) {
   return {description: state.todo.description}
 }
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({changeDescription},dispatch)
+const mapDispatchToProps = (dispatch) => bindActionCreators({changeDescription, search},dispatch)
 
 export default connect(mapStateToProps,mapDispatchToProps)(TodoForm)
